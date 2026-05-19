783259d8be6cc414634b6eacceac582efe56df10
Kseniya Kuchma <kvk7887@yandex.ru>
2026-05-19 13:16:19 +0300

    feat(side-menu): nav/footer split and interactive menu styling

--- shared/ui/SideMenu/index.ts
+++ shared/ui/SideMenu/index.ts
@@ -1,6 +1,10 @@
 export { SideMenu } from './side-menu'
 export { SideMenuContent } from './side-menu-content'
 export type { SideMenuContentProps } from './side-menu-content'
+export { SideMenuNav } from './side-menu-nav'
+export type { SideMenuNavProps } from './side-menu-nav'
+export { SideMenuFooter } from './side-menu-footer'
+export type { SideMenuFooterProps } from './side-menu-footer'
 export {
   COPYRIGHT,
   isNavItemActive,
