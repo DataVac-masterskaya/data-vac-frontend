63b558b98014cb806914dfc5753d113426a6be0d
Aleksey Tikhonov <leohar@mail.ru>
2026-05-26 06:39:14 +0300

    fix: change text adaptive and for theme

--- shared/ui/VaccineInfoBlock/VaccineInfoBlock.tsx
+++ shared/ui/VaccineInfoBlock/VaccineInfoBlock.tsx
@@ -9,12 +9,12 @@ export type VaccineInfoBlockProps = {
 export function VaccineInfoBlock({ title, text }: VaccineInfoBlockProps) {
   return (
     <div className="flex flex-col gap-4">
-      <h3 className="text-[22px] font-medium text-[#E30C5C] leading-none">
+      <h3 className="text-lg font-medium text-accent leading-none dark:text-fg md:text-[22px]">
         {title}
       </h3>
       <div className="flex items-center gap-1.5">
         <InfoCircleIcon width={20} height={20} className="text-[#FB9A40]" />
-        <span className="text-base font-semibold text-[#323335]">
+        <span className="text-base font-semibold text-fg">
           {text}
         </span>
       </div>
