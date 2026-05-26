8c67005d7d9693bc7dbed179f09fed372195d35f
Aleksey Tikhonov <leohar@mail.ru>
2026-05-26 05:53:48 +0300

    fix: rename text and delete flex

--- shared/ui/DonationReasonCard/DonationReasonCard.tsx
+++ shared/ui/DonationReasonCard/DonationReasonCard.tsx
@@ -5,11 +5,11 @@ export type DonationReasonCardProps = {
 
 export function DonationReasonCard({ title, description }: DonationReasonCardProps) {
   return (
-    <div className="bg-white rounded-2xl p-4 flex-1">
-      <h3 className="text-[18px] font-medium text-[#E30C5C] mb-8 leading-none">
+    <div className="bg-white rounded-2xl p-4">
+      <h3 className="text-lg font-medium text-accent mb-8 leading-none">
         {title}
       </h3>
-      <p className="text-[14px] text-[#323237]">
+      <p className="text-sm text-fg">
         {description}
       </p>
     </div>
