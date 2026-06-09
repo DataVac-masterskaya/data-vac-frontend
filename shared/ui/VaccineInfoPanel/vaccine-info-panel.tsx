d6e04b699e4a9c36acc0f7f018f1505401d94095
Maxim Ivanov <makes5712@gmail.com>
2026-06-09 10:47:33 +0300

    fix(vaccine-info-panel): font-normal instead of font-regular, conditional question render

--- shared/ui/VaccineInfoPanel/vaccine-info-panel.tsx
+++ shared/ui/VaccineInfoPanel/vaccine-info-panel.tsx
@@ -20,10 +20,12 @@ export function VaccineInfoPanel({
       title={title}
     >
       <div className="flex flex-col gap-y-5">
-        <p className="font-medium text-[16px] leading-[1.25] text-fg-secondary">
+        {question && (
+          <p className="font-medium text-[16px] leading-[1.25] text-fg-secondary">
             {question}
-        </p>
-        <p className="font-regular text-[16px] leading-[1.25] text-fg">
+          </p>
+        )}
+        <p className="font-normal text-[16px] leading-[1.25] text-fg">
           {explanation}
         </p>
       </div>
