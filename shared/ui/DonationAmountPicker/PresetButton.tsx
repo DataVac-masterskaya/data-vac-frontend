26bf252fdb90b6df6e414915474199f7fc094436
Nadezhda Martin <nadej-martin@yandex.ru>
2026-06-18 11:06:27 +0300

    fix(donation-amount-picker): fix color, font-weight, logic isMonthly

--- shared/ui/DonationAmountPicker/PresetButton.tsx
+++ shared/ui/DonationAmountPicker/PresetButton.tsx
@@ -12,24 +12,28 @@ export function PresetButton({ sum, popular, isSelected, onClick }: PresetButton
     <button
       onClick={onClick}
       className={`py-5 w-full rounded-xl flex items-center justify-center ${
-        popular ? 'bg-neutral pt-5 pb-1' : 'bg-page'
-      }`}
+        isSelected ? 'bg-neutral' : 'bg-subtle'
+      } ${popular ? 'pt-5 pb-1' : ''}`}
     >
       <div className={popular ? 'flex flex-col' : ''}>
         <span
           className={cn(
             'text-xl font-semibold leading-[1.2] hover:text-accent-hover',
             {
-              'text-white': popular,
-              'text-fg': !popular && !isSelected,
-              'text-accent': isSelected,
+              'text-white': isSelected,
+              'text-accent': popular && !isSelected,
+              'text-fg hover:text-accent': !popular && !isSelected,
             }
           )}
         >
           {sum.toLocaleString('ru-RU')}₽
         </span>
         {popular && (
-          <span className="text-xs font-regular leading-[1.3] text-white/50">
+          <span
+            className={`text-xs font-normal leading-[1.3] ${
+              isSelected ? 'text-white/50' : 'text-fg-muted'
+            }`}
+          >
             Популярный
           </span>
         )}
