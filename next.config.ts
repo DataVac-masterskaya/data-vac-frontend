5ac68fea2f458ca958564c4800ba99086c61a9bb
Kseniya Kuchma <kvk7887@yandex.ru>
2026-06-24 14:30:46 +0300

    test(e2e): add VaccinesPage list and row navigation test

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    output: 'standalone',
+  output: 'standalone',
+  allowedDevOrigins: ['127.0.0.1', 'localhost'],
 };
 
 export default nextConfig;
