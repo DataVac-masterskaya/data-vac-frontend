8711fbfac5657f1af5eb1e3a13c7ab0d181cb62e
Kseniya Kuchma <kvk7887@yandex.ru>
2026-06-25 08:55:58 +0300

    style: align quotes and semicolons in PR files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
   allowedDevOrigins: ['127.0.0.1', 'localhost'],
-};
+}
 
-export default nextConfig;
+export default nextConfig
