68d58d3308d44915a6d9532bdbb7d995e8466fb1
DaryaSerbina <daria.srbn@yandex.ru>
2026-07-07 19:47:10 +0300

    add HomePage
    	On branch feat/add-HomePage
    	Changes to be committed:
    	modified:   package-lock.json
    	modified:   page-components/home/index.tsx
    	modified:   shared/ui/Header/Header.tsx
    	modified:   shared/ui/Header/header-nav-row.tsx
    	modified:   shared/ui/PageLayout/PageLayout.tsx

--- page-components/home/index.tsx
+++ page-components/home/index.tsx
@@ -4,19 +4,21 @@ import { fetchIngredients } from '@/shared/api/ingredients'
 import { fetchVaccines } from '@/shared/api/vaccines'
 import { CategoryCard } from './ui/category-card/category-card'
 import { DECORATIVE_SHAPES } from '@/shared/ui/decorative-shapes'
+
 export default async function HomePage() {
   const [vaccines, infections, ingredients, contraindications] = await Promise.all([
-    fetchVaccines({ sort: 'popularity', limit: 5 }),
-    fetchInfections({ sort: 'name_asc', limit: 5 }),
-    fetchIngredients({ sort: 'popularity', limit: 5 }),
-    fetchContraindications({ sort: 'popularity', limit: 5 }),
+    fetchVaccines({ sort: 'popularity', limit: 10 }),
+    fetchInfections({ sort: 'name_desc', limit: 10 }),
+    fetchIngredients({ sort: 'popularity', limit: 10 }),
+    fetchContraindications({ sort: 'popularity', limit: 10 }),
   ])
 
-  const sections = [
+    const sections = [
     {
       title: 'Вакцины',
       viewAllHref: '/vaccines',
       decorationImage: DECORATIVE_SHAPES.bumpy.src,
+      layout: '2col' as const,
       items: vaccines.results.map((v) => ({
         id: v.id,
         name: v.name,
@@ -28,6 +30,7 @@ export default async function HomePage() {
       title: 'Инфекции',
       viewAllHref: '/infections',
       decorationImage: DECORATIVE_SHAPES.porous.src,
+      layout: '2col' as const,
       items: infections.results.map((v) => ({
         id: v.id,
         name: v.name,
@@ -36,20 +39,10 @@ export default async function HomePage() {
       })),
     },
     {
-      title: 'Ингредиенты',
-      viewAllHref: '/ingredients',
-      decorationImage: DECORATIVE_SHAPES.cluster.src,
-      items: ingredients.results.map((v) => ({
-        id: v.id,
-        name: v.name,
-        popularity: v.popularity,
-        href: `/ingredients/${v.id}`,
-      })),
-    },
-    {
       title: 'Противопоказания',
       viewAllHref: '/contraindications',
       decorationImage: DECORATIVE_SHAPES.layered.src,
+      layout: '1col' as const, // <- только здесь 1 колонка
       items: contraindications.results.map((v) => ({
         id: v.id,
         name: v.name,
@@ -57,22 +50,34 @@ export default async function HomePage() {
         href: `/contraindications/${v.id}`,
       })),
     },
+    {
+      title: 'Ингредиенты',
+      viewAllHref: '/ingredients',
+      decorationImage: DECORATIVE_SHAPES.cluster.src,
+      layout: '2col' as const,
+      items: ingredients.results.map((v) => ({
+        id: v.id,
+        name: v.name,
+        popularity: v.popularity,
+        href: `/ingredients/${v.id}`,
+      })),
+    },
   ]
-
-  return (
-      <div>
-        <div className="grid grid-cols-1 min-[500px]:grid-cols-2 gap-6">
-          {sections.map(({ title, viewAllHref, decorationImage, items }) => (
-            <CategoryCard
-              key={title}
-              title={title}
-              viewAllHref={viewAllHref}
-              decorationImage={decorationImage}
-              items={items}
-              layout="1col"
-            />
-          ))}
-        </div>
+     return (
+      <div className="grid grid-cols-1 min-[500px]:grid-cols-2 w-full
+      gap-2 
+      md:!gap-3 
+      xl:!gap-5">
+        {sections.map(({ title, viewAllHref, decorationImage, items,layout }) => (
+          <CategoryCard
+            key={title}
+            title={title}
+            viewAllHref={viewAllHref}
+            decorationImage={decorationImage}
+            items={items}
+             layout={layout}
+          />
+        ))}
       </div>
   )
 }
