524228d965a16680331e59f31e129fe8757b0b93
Nadezhda Martin <nadej-martin@yandex.ru>
2026-07-07 15:42:31 +0300

    refactor(vaccine-catalog-page): add component ResultsHeader

--- page-components/vaccines/list/index.tsx
+++ page-components/vaccines/list/index.tsx
@@ -1,11 +1,9 @@
-import { BackLink } from "@/shared/ui/back-link"
 import { VaccineAlphabetFilter } from "./VaccineAlphabetFilter"
-import { Separator } from "@/shared/ui/separator"
 import { VaccineCatalog } from "../ui/VaccineCatalog"
 import { fetchVaccines } from "@/shared/api/vaccines"
 import { Suspense } from "react"
 import { mapVaccineToTableRowOnPageVaccine } from "../lib/map-vaccine-to-table-row"
-import { resultsLabel } from "@/shared/lib/pluralize"
+import { ResultsHeader } from "@/shared/ui/ResultsHeader"
 
 export default async function VaccineCatalogPage({
   searchParams,
@@ -22,19 +20,15 @@ export default async function VaccineCatalogPage({
 
   return (
     <div>
-      <div className="flex flex-col gap-y-4">
-        <BackLink href="/" />
-        <h1 className="text-2xl font-normal text-fg">Вакцины</h1>
-        <div className="flex flex-col items-start gap-[19px] md:!flex-row md:!justify-between md:!items-center">
+      <ResultsHeader
+        title="Вакцины"
+        count={count}
+        filters={
           <Suspense fallback={null}>
             <VaccineAlphabetFilter />
           </Suspense>
-          <span className="text-fg-secondary font-normal text-base">{resultsLabel(count)}</span>
-        </div>
-        <Separator/>
-      </div>
-      <div className="mt-4">
-      </div>
+        }
+      />
       <div className="mt-6">
         {vaccines.length === 0 ? (
           <div className="mt-4 bg-card rounded-2xl p-10 text-center text-fg-secondary">
