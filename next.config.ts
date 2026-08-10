76c1e8274b338d0ac8dbe0177a8af590620d88dc
Maxim Ivanov <makes5712@gmail.com>
2026-08-10 16:27:52 +0300

    feat: enhance API error handling and logging in fetch utilities

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'standalone',
   allowedDevOrigins: ['127.0.0.1', 'localhost'],
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 }
 
 export default nextConfig
