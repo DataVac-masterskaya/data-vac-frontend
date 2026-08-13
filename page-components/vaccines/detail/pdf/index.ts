991037c9d6b1264f80fafcc12688145935ccac0d
Kseniya Kuchma <kvk7887@yandex.ru>
2026-08-13 15:21:24 +0300

    feat(vaccine-pdf): add pdf document template and download utility

--- page-components/vaccines/detail/pdf/index.ts
+++ page-components/vaccines/detail/pdf/index.ts
@@ -1,2 +1,4 @@
 export type { VaccinePdfData } from './vaccine-pdf.types'
 export { mapVaccineToPdfData } from './map-vaccine-to-pdf-data'
+export { VaccinePdfDocument } from './vaccine-pdf-document'
+export { downloadVaccinePdf } from './download-vaccine-pdf'
